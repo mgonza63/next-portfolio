64dface7b6d317bb86a000f57fc0103193a5ad6d
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-05-14 21:58:36 -0500

    added call to action

--- components/HeroSection.js
+++ components/HeroSection.js
@@ -3,6 +3,7 @@ function Hero() {
         <article className="container">
             <h1 className="hero">Crea tu Comumidad Digital</h1>
             <h5 className="subhero">Desarolla tu presencia en linea con facilidad y efectividad</h5>
+            <button className="action"><p>Consulta gratuita</p></button>
             
             <style jsx>{`
                 .container {
@@ -22,6 +23,29 @@ function Hero() {
                     font-weight: lighter;
                     font-size: 20px;  
                 }
+                button {
+                    position: relative;
+                    top: 22vh;
+                }
+                p {
+                    color: rgb(166, 168, 172);
+                }
+                button:hover {
+                    background-color: rgb(209, 152, 29);
+                }
+                button:hover p {
+                    color: #201f1f;
+                }
+                
+                @media only screen and (max-width: 600px) {
+                    .hero  {
+                        top: 30%;
+                    }
+                    .subhero {
+                        top: 32%;
+                    }
+                }
+                
 
             `}
             </style>
