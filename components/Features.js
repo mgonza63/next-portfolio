d2b8f5b617fec5f02bd5e0ba00adaee378f8da25
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-05-26 17:14:08 -0500

    added dinamic components that render on click with useState hook

--- components/Features.js
+++ components/Features.js
@@ -1,8 +1,26 @@
+import { useState } from 'react';
+
+
 function Features() {
+    // const packages 
+    const [state, setState] = useState({
+            title: 'Redes Sociales',
+            description: 'Creamos perfiles de tu empresa en las redes sociales más populares y te creamos contenido de acuerdo a ti y a tu mercado para darle la identidad necesaria y sacarle ventaja a la competencia.'
+    });
+
+
     return (
         <div className="container wrapper">
             <div className="row">
                 <div className="four columns">
+                    <div className="card" onClick={() => setState({ ...state, title: 'Redes Sociales', description: 'Arranca tu presencia en linea y sacale ventaja a la competencia con contenido de calidad creado por nosotros de acuerdo a lo que buscas.'})}>
+                            <img src="/logo.png" alt="logo"></img>
+                            <div className="content">
+                                <p>Arranca tu presencia online con el Paquete de Redes Sociales</p>
+                            </div>
+                    </div>
+                </div>
+                <div className="four columns" onClick={() => setState({ ...state, title: "Community Manager", description: "Creamos Campañas publicitarias para hacerte llegar al cliente ideal y lograr expandir tus ventas, interactuamos con los seguidores y desarrollamos contenido digital de calidad para que tus redes estén activas y no pierdan presencia en el mercado." })}>
                     <div className="card">
                         <img src="/logo.png" alt="logo"></img>
                             <div className="content">
@@ -11,21 +29,19 @@ function Features() {
                     </div>
                 </div>
                 <div className="four columns">
-                    <div className="card">
+                    <div className="card" onClick={() => setState({ ...state, title: "Página Web", description: "Desarrollamos tu página web desde cero de acuerdo a ttus gustos y necesidades que te ayudarán a mantenerte a la vista y en contacto de tus clientes y seguidores. Nuestro compromiso es entregarte una página profesional y dinámica." })}>
                         <img src="/logo.png" alt="logo"></img>
                             <div className="content">
                                 <p>Arranca tu presencia online con el Paquete de Redes Sociales</p>
                             </div>
                     </div>
                 </div>
-                <div className="four columns">
-                    <div className="card">
-                        <img src="/logo.png" alt="logo"></img>
-                            <div className="content">
-                                <p>Arranca tu presencia online con el Paquete de Redes Sociales</p>
-                            </div>
-                    </div>
+            <div className="row">
+                <div className="twelve columns">
+                    <h3 className="cardTitle">{state.title}</h3>
+                    <p className="description">{state.description}</p>
                 </div>
+            </div>
 
 
             </div>
@@ -46,12 +62,18 @@ function Features() {
                     flex: 1;
         /* Space between columns */
                     margin: 0 8px;
-                    padding: 10px
+                    padding: 10px;
+                    -webkit-transition-duration: 0.3s;
+                    transition-duration: 0.3s;
+                    -webkit-transition-property: transform;
+                    transition-property: transform;
+                    cursor: pointer;
 
         /* Layout each column */
                 }
                 .content {
                     flex: 1;
+                    
                 }
                 img {
                     width: 50px;
@@ -59,7 +81,21 @@ function Features() {
                     margin-left: auto;
                     margin-right: auto;
                     padding-bottom: 10px;
+                    padding-top: 10px;
+                    cursor: pointer;
+                    -webkit-transition-duration: 0.3s;
+                    transition-duration: 0.3s;
+                    -webkit-transition-property: transform;
+                    transition-property: transform;
 
+                }
+                .cardTitle, .description {
+                    padding-left: 15px;
+                }
+
+                .card:hover img{
+                    -webkit-transform: scale(1.1);
+                    transform: scale(1.1);
                 }
                 @media only screen and (max-width: 600px) {
                     .wrapper {
