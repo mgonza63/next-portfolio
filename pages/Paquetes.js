2fc79f523e8c78828e92f7972d0dd70c7e81d200
Mauricio Gonzalez <mauglezg21@gmail.com>
2020-05-28 19:06:55 -0500

    fixed the paquetes url

--- pages/Paquetes.js
+++ pages/Paquetes.js
@@ -10,12 +10,14 @@ export default function Paquetes() {
     return (
         <Layout>
             <Head>
-                <title>Gosify Media</title>
-                <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <title>Gosify Media | Marketing Digital</title>
+            <meta charset="utf-8"></meta>
+            <meta name="viewport" content="initial-scale=1.0, width=device-width" />
+            <meta name="description" content="Gosify es una agencia de Marketing Digital, trabaja con nosotros para maximizar tus ventas y mejorar tu presencia en linea"></meta>
 
-                <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"></link>
-                <link href="https://fonts.googleapis.com/css2?family=DM+Mono&family=Playfair+Display:wght@500&display=swap" rel="stylesheet"></link>
-                <script src="https://code.iconify.design/1/1.0.6/iconify.min.js"></script>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/skeleton/2.0.4/skeleton.min.css"></link>
+            <link href="https://fonts.googleapis.com/css2?family=DM+Mono&family=Playfair+Display:wght@500&display=swap" rel="stylesheet"></link>
+    
             </Head>
             <Header />
             <PackageSection />
